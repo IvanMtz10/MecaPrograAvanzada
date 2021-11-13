6c712230bcd107aa01a4431f8c21de43c337ac4c
IvanMtz10 <martinez.nava.ivan.cbtis44@gmail.com>
2021-11-13 15:18:03 -0600

    Clase persona completa

--- Evaluacion.py
+++ Evaluacion.py
@@ -1,24 +1,38 @@
 class Persona:
-    def __init__(self, nombre, edad,altura):
+    def __init__(self, nombre,apellidoP,apellidoM, edad,altura,noTelefono):
         self.__nombre=nombre
+        self.__apellidoP=apellidoP
+        self.__apellidoM=apellidoM
         self.__edad=edad
         self.__altura=altura
-
-    def getNombre(self):
+        self.__noTelefono=noTelefono
+#Getters y setters
+    def __getNombre(self):
         return self.__nombre
-    def getEdad(self):
+    def __getApellidoP(self):
+        return self.__apellidoP
+    def __getApellidoM(self):
+        return self.__apellidoM
+    def __getEdad(self):
         return self.__edad
-    def getAltura(self):
+    def __getAltura(self):
         return self.__altura
+    def __getTelefono
+        return self.__noTelefono
 
     def setEdad(self, nuevaEdad):
         self.__edad=nuevaEdad
     def setAltura(self, nuevaAltura):
         self.__altura=nuevaAltura
+    def setTelefono(self,nuevoTelefono):
+        self.__noTelefono=nuevoTelefono
+
+    def presentacion(self):
+        pass
 
 class Empleado(Persona):
-    def __init__(self, nombre, edad,altura, cargo):
-        super().__init__(nombre, edad,altura)
+    def __init__(self,nombre,apellidoP,apellidoM, edad,altura,noTelefono, cargo):
+        super().__init__(nombre,apellidoP,apellidoM, edad,altura,noTelefono)
         self.__cargo =cargo
 
     def getCargo(self):
--- test1.py
+++ test1.py
@@ -5,3 +5,4 @@ Estos son los cambios realizados desde Pycharm
 print("Archivo modificado desde pycharm")
 
 print("EStos cambios son para la rama main")
+
