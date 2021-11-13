576338cda9edff177c0625b7ef40e9a082259c9e
IvanMtz10 <martinez.nava.ivan.cbtis44@gmail.com>
2021-11-13 15:28:23 -0600

    Agregando atributos a la clase empleado

--- Evaluacion.py
+++ Evaluacion.py
@@ -1,11 +1,12 @@
 class Persona:
-    def __init__(self, nombre,apellidoP,apellidoM, edad,altura,noTelefono):
+    def __init__(self, nombre,apellidoP,apellidoM, edad,altura,noTelefono,carrera):
         self.__nombre=nombre
         self.__apellidoP=apellidoP
         self.__apellidoM=apellidoM
         self.__edad=edad
         self.__altura=altura
         self.__noTelefono=noTelefono
+        self.__carrera=carrera
 #Getters y setters
     def __getNombre(self):
         return self.__nombre
@@ -17,9 +18,15 @@ class Persona:
         return self.__edad
     def __getAltura(self):
         return self.__altura
-    def __getTelefono
+    def __getTelefono(self):
         return self.__noTelefono
 
+    def setNombre(self, nombreCorrecto):
+        self.__nombre=nombreCorrecto
+    def setApellidoP(self,apellidoPCorrecto):
+        self.__apellidoP=apellidoPCorrecto
+    def setApellidoM(self,apellidoMCorrecto):
+        self.__apellidoM=apellidoMCorrecto
     def setEdad(self, nuevaEdad):
         self.__edad=nuevaEdad
     def setAltura(self, nuevaAltura):
@@ -31,12 +38,19 @@ class Persona:
         pass
 
 class Empleado(Persona):
-    def __init__(self,nombre,apellidoP,apellidoM, edad,altura,noTelefono, cargo):
-        super().__init__(nombre,apellidoP,apellidoM, edad,altura,noTelefono)
+    def __init__(self,nombre,apellidoP,apellidoM, edad,altura,noTelefono,carrera, empresa, cargo, sueldo):
+        super().__init__(nombre,apellidoP,apellidoM, edad,altura,noTelefono,carrera)
         self.__cargo =cargo
+        self.__empresa=empresa
+        self.__sueldo=sueldo
 
+#setters y getters
+    def getEmpresa(self):
+        return self.__empresa
     def getCargo(self):
         return self.__cargo
+    def getSueldo(self):
+        return self.__sueldo
 
     def setCargo(self, nuevoCargo):
         self.__cargo=nuevoCargo
